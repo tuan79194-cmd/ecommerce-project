786473857527cd1a04ab4fddc1620716a024b245
Tu Le <tuan79194@gmail.com>
2026-02-25 10:26:16 +0700

    054920-move checkout page into React-1025AM-25022026

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
+import { Checkoutpage } from './pages/CheckoutPage';
 import './App.css'
 
 function App() {
@@ -7,12 +8,13 @@ function App() {
   return (
     <Routes>
         {/* index does the same thing as path="/" */}
-        <Route index element={<HomePage />} />
+        <Route 
+            index 
+            element={<HomePage />} />
 
-        <Route path="checkout" element={
-            <div>Test checkout page</div>
-        } />
-        
+        <Route 
+            path="checkout" 
+            element={<Checkoutpage />} />
     </Routes>
   )
 }
