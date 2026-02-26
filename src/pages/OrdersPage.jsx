1509b5a42add8bd8ba43a66407de1bbab1263e68
Tu Le <tuan79194@gmail.com>
2026-02-26 10:39:31 +0700

    6i-<a> to <Link>-1039AM-2602026

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from '../components/Header';
+import { Link } from "react-router";
 import './OrdersPage.css';
 
 export function OrdersPage() {
@@ -136,11 +137,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
 import "./TrackingPage.css";
+import { Link } from "react-router";
 export function TrackingPage() {
     return (
         <>
@@ -11,9 +12,9 @@ export function TrackingPage() {
 
             <div className="tracking-page">
                 <div className="order-tracking">
-                    <a className="back-to-orders-link link-primary" href="/orders">
+                    <Link className="back-to-orders-link link-primary" to="/orders">
                         View all orders
-                    </a>
+                    </Link>
 
                     <div className="delivery-date">
                         Arriving on Monday, June 13
